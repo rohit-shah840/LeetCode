9dffee92532bced5ee0d11ebf2132ff9e2166a1c
rohit-shah840 <rohitshah1455@gmail.com>
2022-07-29 21:10:19 +0530

    leetcode

---
+++ integerToString.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class integerToString {
+    static public String intToRoman(int n) {
+        int[] a = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+        String[] s = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+        StringBuilder str = new StringBuilder();
+
+        for (int i = 0; i < a.length; i++) {
+            while (n >= a[i]) {
+                str.append(s[i]);
+                n -= a[i];
+            }
+        }
+        return str.toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number to be converted into roman :");
+        int num = sc.nextInt();
+        System.out.println("The roman form of this number " + num + " is " + intToRoman(num));
+    }
+}
