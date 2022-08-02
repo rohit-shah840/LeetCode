3497bb70abb8becf38610b2f8beb6556b4aaf6ac
rohit-shah840 <rohitshah1455@gmail.com>
2022-08-02 23:07:36 +0530

    leetproblem

--- ConcatenationOfArray.java
+++ ConcatenationOfArray.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
 
-import javax.crypto.spec.GCMParameterSpec;
-
 public class ConcatenationOfArray {
     static public int[] getConcatenation(int[] nums) {
         int l = nums.length;
