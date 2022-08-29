4d43324f99188d11d09ce3aa9aae9793646e24e2
Rohit Shah <96192862+rohit-shah840@users.noreply.github.com>
2022-08-29 23:56:39 +0530

    Time: 0 ms (100.00%), Space: 39.1 MB (98.64%) - LeetHub

---
+++ 172-factorial-trailing-zeroes/172-factorial-trailing-zeroes.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int trailingZeroes(int n) {
+        if(n==0)  return n;
+        int sum=0;
+        while(n!=0){
+            sum+=n/5;
+            n/=5;
+        }
+        return sum;
+    }
+}
