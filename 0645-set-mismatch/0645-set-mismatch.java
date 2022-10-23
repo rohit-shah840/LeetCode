f344d9b998e6a75d70645591eea6dc17f4300469
Rohit Shah <96192862+rohit-shah840@users.noreply.github.com>
2022-10-23 10:55:52 +0530

    Time: 1 ms (100.00%), Space: 44 MB (89.51%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,18 +1,18 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
-        int arr[]=new int[2];
-        int n=nums.length;
-        int sum=(n*(n+1)/2);
-        int sum1=0;
-        for(int i=0;i<n;i++)sum1+=nums[i];
-        for(int i=0;i<n-1;i++){
-            for(int j=i+1;j<n;j++){
-                if(nums[i]==nums[j]){
-                    arr[0]=nums[i];
-                    arr[1]=Math.abs(sum-(sum1-nums[i]));
-                }
+        int arr[]=new int[nums.length];
+       int ans[]=  new int[2];
+        for(int a : nums){
+            arr[a-1]++;
+        }
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==2){
+                ans[0]=i+1;
+            }
+            if(arr[i]==0){
+                ans[1]=i+1;
             }
         }
-        return arr;
+        return ans;
     }
 }
