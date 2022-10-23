3cdfe48f2c7a7c407d61306ed30a92417e988819
Rohit Shah <96192862+rohit-shah840@users.noreply.github.com>
2022-10-23 19:28:01 +0530

    Time: 0 ms (100.00%), Space: 43.3 MB (56.62%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,11 +1,23 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int i;
-        for(i=0;i<nums.length;i++){
-            if(target<=nums[i]){
-                return i;
-            }
+        //int i;
+        //TC = O(n)
+        // for(i=0;i<nums.length;i++){
+        //     if(target<=nums[i]){
+        //         return i;
+        //     }
+        // }
+        int l=0;
+        int h=nums.length -1;
+        while(l<=h){
+            int mid = l+(h-l)/2;
+            if(nums[mid]==target)return mid;
+               if(nums[mid]>target){
+                   h=mid-1;
+               } else{
+                   l=mid+1;
+               }
         }
-        return i;
+         return l;
     }
 }
