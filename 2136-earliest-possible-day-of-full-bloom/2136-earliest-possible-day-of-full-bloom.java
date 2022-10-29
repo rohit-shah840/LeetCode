10d86d6786de398fc4a15b9dc10406b1fc976874
Rohit Shah <96192862+rohit-shah840@users.noreply.github.com>
2022-10-29 11:07:07 +0530

    Time: 192 ms (18.27%), Space: 122.8 MB (62.10%) - LeetHub

--- 2136-earliest-possible-day-of-full-bloom/2136-earliest-possible-day-of-full-bloom.java
+++ 2136-earliest-possible-day-of-full-bloom/2136-earliest-possible-day-of-full-bloom.java
@@ -1,18 +1,20 @@
 class Solution {
     public int earliestFullBloom(int[] plantTime, int[] growTime) {
          int n = growTime.length;
-        List<Integer> indices = new ArrayList<>();
-        for (int i = 0; i < n; ++i) {
-            indices.add(i);
+         int arr[][]= new int [n][2];
+        for(int i=0;i<n;i++){
+            arr[i][0]=plantTime[i];
+            arr[i][1]=growTime[i];
         }
-        Collections.sort(indices, Comparator.comparingInt(i -> -growTime[i]));
-        int max = 0;
-        for (int i = 0, plantSum = 0; i < n; ++i) {
-            int idx = indices.get(i);
-            int time = plantSum + plantTime[idx] + growTime[idx];
-            max = Math.max(max, time);
-            plantSum += plantTime[idx];
+        
+        Arrays.sort(arr,(a,b)-> b[1] - a[1]);
+        int bloomDay=0,seedPlantingDay=0;
+        for(int i=0;i<n;i++){
+            int ptime=arr[i][0];
+            int gtime=arr[i][1];
+            bloomDay = Math.max(bloomDay,seedPlantingDay+ptime+gtime);
+            seedPlantingDay = seedPlantingDay+ptime;
         }
-        return max;
+        return bloomDay;
     }
 }
