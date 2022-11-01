2b7af0e6e1108d9547bafae1e4178fdd472dd78e
Rohit Shah <96192862+rohit-shah840@users.noreply.github.com>
2022-11-01 23:34:03 +0530

    Time: 10 ms (25.34%), Space: 57.2 MB (79.86%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,19 +1,31 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int result[]= new int[nums.length];
-        int temp=1;
-        //for the left multiple array stored
-        for(int i=0;i<nums.length;i++){
-            result[i]=temp;
-            temp*=nums[i];
-            System.out.print(result[i]);
-        }
-        temp=1;
-        //for the right multiple array stored
-        for(int i=nums.length-1;i>=0;i--){
-            result[i]*=temp;
-            temp*=nums[i];
-        }
-        return result;
+        int n = nums.length;
+    int[] res = new int[n];
+    res[0] = 1;
+    for (int i = 1; i < n; i++) {
+        res[i] = res[i - 1] * nums[i - 1];
+    }
+    int right = 1;
+    for (int i = n - 1; i >= 0; i--) {
+        res[i] *= right;
+        right *= nums[i];
+    }
+    return res;
+        // int result[]= new int[nums.length];
+        // int temp=1;
+        // //for the left multiple array stored
+        // for(int i=0;i<nums.length;i++){
+        //     result[i]=temp;
+        //     temp*=nums[i];
+        //    // System.out.print(result[i]);
+        // }
+        // temp=1;
+        // //for the right multiple array stored
+        // for(int i=nums.length-1;i>=0;i--){
+        //     result[i]*=temp;
+        //     temp*=nums[i];
+        // }
+        // return result;
     }
 }
